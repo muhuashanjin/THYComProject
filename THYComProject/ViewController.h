d7d1f438b5892ab207f85ae21e1c5aaa36287fe9
muhuashanjin <2471327309@qq.com>
2016-12-05 11:19:25 +0800

    Initial Commit

---
+++ THYComProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  THYComProject
+//
+//  Created by thy on 16/12/5.
+//  Copyright © 2016年 thy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ THYComProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  THYComProject
+//
+//  Created by thy on 16/12/5.
+//  Copyright © 2016年 thy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
